dfe43ef0b5961a4d645c0625e968cb1d4f3cafc3
Sebastien Viguier <viguier.sebastien@orange.fr>
2023-09-16 20:58:33 +0200

    nest working in docker

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,9 +2,21 @@ import { MikroORMOptions } from '@mikro-orm/core';
 
 export const config = () => {
   return {
-    env: process.env.ENV || 'local',
+    env: process.env.ENV ?? 'local',
+    network: {
+      host: process.env.HOST ?? '0.0.0.0',
+      port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+      origins: process.env.ORIGINS?.split(',') ?? ['http://localhost:4200'],
+    },
+    openapi: {
+      prefix: 'openapi',
+      title: 'Todos',
+      description: 'Small API of todos',
+      version: '1.0',
+      tags: ['todos'],
+    },
     orm: {
-      clientUrl: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/todos',
+      clientUrl: process.env.DATABASE_URL ?? 'postgresql://postgres:postgres@localhost:5432/todos',
       type: 'postgresql',
     } as MikroORMOptions,
   };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,37 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
+import { config } from './config';
 
 async function bootstrap() {
+  const { network, env, openapi } = config();
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
     bufferLogs: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
-  app.enableCors();
+  app.enableCors({ origin: network.origins });
   app.enableShutdownHooks();
-  await app.listen(3000);
+
+  if (env === 'local') {
+    const openapiConfig = new DocumentBuilder()
+      .setTitle(openapi.title)
+      .setDescription(openapi.description)
+      .setVersion(openapi.version)
+      .build();
+
+    openapiConfig.tags = openapi.tags.map((tag) => ({
+      name: tag,
+    }));
+
+    const document = SwaggerModule.createDocument(app, openapiConfig);
+    SwaggerModule.setup(openapi.prefix, app, document);
+  }
+
+  await app.listen(network.port, network.host);
 }
+
 bootstrap();
