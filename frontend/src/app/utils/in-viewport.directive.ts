c449a5e1533809a6d79483ec76bbf8b6867368b8
Sébastien Viguier <viguier.sebastien@orange.fr>
2024-03-11 13:52:43 +0100

    viewport directive lifecycle

--- frontend/src/app/utils/in-viewport.directive.ts
+++ frontend/src/app/utils/in-viewport.directive.ts
@@ -1,12 +1,20 @@
-import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, OnDestroy, Output } from '@angular/core';
 
 @Directive({
   selector: '[inViewport]',
   standalone: true,
 })
-export class InViewportDirective {
-  private observer: IntersectionObserver;
+export class InViewportDirective implements OnDestroy {
   @Output() visibleInViewport = new EventEmitter<void>();
+
+  private observer: IntersectionObserver;
+
+  private callback: ConstructorParameters<typeof IntersectionObserver>[0] = (entries) =>
+    entries
+      .filter((entry) => entry.isIntersecting)
+      .forEach((_entry) => {
+        this.visibleInViewport.emit();
+      });
 
   constructor(private el: ElementRef) {
     this.observer = new IntersectionObserver(this.callback, {
@@ -17,10 +25,7 @@ export class InViewportDirective {
     this.observer.observe(this.el.nativeElement);
   }
 
-  private callback: ConstructorParameters<typeof IntersectionObserver>[0] = (entries) =>
-    entries
-      .filter((entry) => entry.isIntersecting)
-      .forEach((_entry) => {
-        this.visibleInViewport.emit();
-      });
+  ngOnDestroy(): void {
+    this.observer.disconnect();
+  }
 }
