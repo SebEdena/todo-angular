3b3571ed62893adf511b7fae59895b9d3b59a4e2
Sébastien Viguier <viguier.sebastien@orange.fr>
2024-05-17 15:11:30 +0200

    upgrade angular & migrate to jest

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
-import { MockComponent } from 'ng-mocks';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       providers: [provideRouter([])],
-      imports: [AppComponent, MockComponent(HeaderComponent)],
+      imports: [AppComponent],
     }),
   );
 
--- frontend/src/app/views/todo-detail/todo-detail.component.spec.ts
+++ frontend/src/app/views/todo-detail/todo-detail.component.spec.ts
@@ -1,8 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { readTodo } from '@lib';
-import { MockInstance, MockService } from 'ng-mocks';
-import { ReadTodo } from 'src/app/models/todos';
 import { TodoService } from 'src/app/services/todo.service';
 import { TodoDetailComponent } from './todo-detail.component';
 
@@ -10,15 +7,7 @@ describe('TodoDetailComponent', () => {
   let component: TodoDetailComponent;
   let fixture: ComponentFixture<TodoDetailComponent>;
 
-  MockInstance.scope('case');
-
   beforeEach(() => {
-    MockService(TodoService);
-    MockInstance(TodoService, 'get', async (id: string) => {
-      const todo = readTodo();
-      todo.id = id;
-      return todo as ReadTodo;
-    });
     TestBed.configureTestingModule({
       imports: [TodoDetailComponent],
       providers: [TodoService],
